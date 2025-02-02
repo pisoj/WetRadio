ec6e4eda6ef7bb3f1ffe8443b27590e38e3d4554
pisoj <josip.tismat@gmail.com>
2025-02-02 17:54:44 +0100

    Move recordings to separate page for better SEO

--- public_html/index.php
+++ public_html/index.php
@@ -23,24 +23,6 @@ $preferences_string = $conn->query("SELECT key, value FROM preferences_string")-
 
 <body>
   <iframe title="Status slanja" name="message" class="message-frame" src="data:text/html;charset=utf-8,%3Chtml%3E%3Chead%3E%3Cstyle%3E%3Aroot%7Bcolor%2Dscheme%3A%20dark%3B%7D%3C%2Fstyle%3E%3C%2Fhead%3E%3Cbody%3E%3C%2Fbody%3E%3C%2Fhtml%3E"></iframe>
-  <?php
-  $items_stmt = $conn->prepare("SELECT id, title, subtitle, image, is_replayable, category_id FROM show_items WHERE disabled = 0 ORDER BY priority DESC");
-  $items_stmt->execute();
-  $show_items = $items_stmt->fetchAll(PDO::FETCH_OBJ);
-  foreach ($show_items as $item) {
-    if (!$item->is_replayable)
-      continue;
-    echo "
-    <div id=\"recordings-{$item->id}\" class=\"window\">
-      <div class=\"content\">
-        <h4>{$item->title}</h4>" .
-      ($item->is_replayable ? "<a href=\"#show-{$item->id}\" class=\"icon-button icon\" aria-label=\"Snimke\"><svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 384 512\"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d=\"M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z\"/></svg></a>" : "") .
-      "</div>
-      <iframe src=\"recordings.php?id={$item->id}\" loading=\"lazy\" title=\"Snimke emisije: {$item->title}\"></iframe>
-    </div>
-    ";
-  }
-  ?>
   <div id="recorder" class="window">
     <div class="recorder" data-status="init">
       <div class="main">
@@ -167,6 +149,9 @@ $preferences_string = $conn->query("SELECT key, value FROM preferences_string")-
     <?php
     $category_stmt = $conn->prepare("SELECT id, title FROM show_categories WHERE disabled = 0 ORDER BY priority DESC");
     $category_stmt->execute();
+    $items_stmt = $conn->prepare("SELECT id, title, subtitle, image, is_replayable, category_id FROM show_items WHERE disabled = 0 ORDER BY priority DESC");
+    $items_stmt->execute();
+    $show_items = $items_stmt->fetchAll(PDO::FETCH_OBJ);
     while ($category = $category_stmt->fetchObject()) {
       echo "
       <h3>{$category->title}</h3>
@@ -183,7 +168,7 @@ $preferences_string = $conn->query("SELECT key, value FROM preferences_string")-
               <h4>{$item->title}</h4>" .
           ($item->subtitle ? "<p class=\"text-muted\">{$item->subtitle}</p>" : "") .
           "</div>" .
-          ($item->is_replayable ? "<a href=\"#recordings-{$item->id}\" class=\"icon-button icon\" aria-label=\"Snimke\"><svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 384 512\"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d=\"M64 0C28.7 0 0 28.7 0 64L0 448c0 35.3 28.7 64 64 64l256 0c35.3 0 64-28.7 64-64l0-288-128 0c-17.7 0-32-14.3-32-32L224 0 64 0zM256 0l0 128 128 0L256 0zm2 226.3c37.1 22.4 62 63.1 62 109.7s-24.9 87.3-62 109.7c-7.6 4.6-17.4 2.1-22-5.4s-2.1-17.4 5.4-22C269.4 401.5 288 370.9 288 336s-18.6-65.5-46.5-82.3c-7.6-4.6-10-14.4-5.4-22s14.4-10 22-5.4zm-91.9 30.9c6 2.5 9.9 8.3 9.9 14.8l0 128c0 6.5-3.9 12.3-9.9 14.8s-12.9 1.1-17.4-3.5L113.4 376 80 376c-8.8 0-16-7.2-16-16l0-48c0-8.8 7.2-16 16-16l33.4 0 35.3-35.3c4.6-4.6 11.5-5.9 17.4-3.5zm51 34.9c6.6-5.9 16.7-5.3 22.6 1.3C249.8 304.6 256 319.6 256 336s-6.2 31.4-16.3 42.7c-5.9 6.6-16 7.1-22.6 1.3s-7.1-16-1.3-22.6c5.1-5.7 8.1-13.1 8.1-21.3s-3.1-15.7-8.1-21.3c-5.9-6.6-5.3-16.7 1.3-22.6z\"/></svg></a>" : "") .
+          ($item->is_replayable ? "<a href=\"recordings.php?id={$item->id}\" class=\"icon-button icon\" aria-label=\"Snimke\"><svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 384 512\"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d=\"M64 0C28.7 0 0 28.7 0 64L0 448c0 35.3 28.7 64 64 64l256 0c35.3 0 64-28.7 64-64l0-288-128 0c-17.7 0-32-14.3-32-32L224 0 64 0zM256 0l0 128 128 0L256 0zm2 226.3c37.1 22.4 62 63.1 62 109.7s-24.9 87.3-62 109.7c-7.6 4.6-17.4 2.1-22-5.4s-2.1-17.4 5.4-22C269.4 401.5 288 370.9 288 336s-18.6-65.5-46.5-82.3c-7.6-4.6-10-14.4-5.4-22s14.4-10 22-5.4zm-91.9 30.9c6 2.5 9.9 8.3 9.9 14.8l0 128c0 6.5-3.9 12.3-9.9 14.8s-12.9 1.1-17.4-3.5L113.4 376 80 376c-8.8 0-16-7.2-16-16l0-48c0-8.8 7.2-16 16-16l33.4 0 35.3-35.3c4.6-4.6 11.5-5.9 17.4-3.5zm51 34.9c6.6-5.9 16.7-5.3 22.6 1.3C249.8 304.6 256 319.6 256 336s-6.2 31.4-16.3 42.7c-5.9 6.6-16 7.1-22.6 1.3s-7.1-16-1.3-22.6c5.1-5.7 8.1-13.1 8.1-21.3s-3.1-15.7-8.1-21.3c-5.9-6.6-5.3-16.7 1.3-22.6z\"/></svg></a>" : "") .
           "</div>
         </div>
         ";
